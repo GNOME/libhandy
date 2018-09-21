5b06cd6521749ff71c75f3e13ac9bdbff4104faa
Guido Günther <agx@sigxcpu.org>
2018-09-21 10:20:23 +0200

    HdyFold: Use glib-mkenums
    This makes the enums clickable in the HdyLeaflet documentation,
    HdyFold usable in GtkBuild and makes the code smaller.

--- src/hdy-fold.h
+++ src/hdy-fold.h
@@ -15,14 +15,10 @@
 
 G_BEGIN_DECLS
 
-#define HDY_TYPE_FOLD (hdy_fold_get_type ())
-
 typedef enum {
   HDY_FOLD_UNFOLDED,
   HDY_FOLD_FOLDED,
 } HdyFold;
-
-GType hdy_fold_get_type (void);
 
 G_END_DECLS
 
