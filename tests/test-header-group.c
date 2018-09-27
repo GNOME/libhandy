bf79fe866d53ac1f67337fc98725120cd91119a8
Guido Günther <agx@sigxcpu.org>
2018-09-27 12:49:06 +0200

    HdyHeaderGroup: Allow to get and remove the headerbars

--- tests/test-header-group.c
+++ tests/test-header-group.c
@@ -33,6 +33,35 @@ test_hdy_header_group_focus (void)
 }
 
 
+static void
+test_hdy_header_group_add_remove (void)
+{
+  HdyHeaderGroup *hg;
+  GtkHeaderBar *bar1, *bar2;
+
+  hg = HDY_HEADER_GROUP (hdy_header_group_new ());
+
+  bar1 = GTK_HEADER_BAR (gtk_header_bar_new ());
+  bar2 = GTK_HEADER_BAR (gtk_header_bar_new ());
+
+  g_assert_cmpint (g_slist_length (hdy_header_group_get_header_bars (hg)), ==, 0);
+
+  hdy_header_group_add_header_bar (hg, GTK_HEADER_BAR (bar1));
+  g_assert_cmpint (g_slist_length (hdy_header_group_get_header_bars (hg)), ==, 1);
+
+  hdy_header_group_add_header_bar (hg, GTK_HEADER_BAR (bar2));
+  g_assert_cmpint (g_slist_length (hdy_header_group_get_header_bars (hg)), ==, 2);
+
+  hdy_header_group_remove_header_bar (hg, GTK_HEADER_BAR (bar2));
+  g_assert_cmpint (g_slist_length (hdy_header_group_get_header_bars (hg)), ==, 1);
+
+  hdy_header_group_remove_header_bar (hg, GTK_HEADER_BAR (bar1));
+  g_assert_cmpint (g_slist_length (hdy_header_group_get_header_bars (hg)), ==, 0);
+
+  g_object_unref (hg);
+}
+
+
 gint
 main (gint argc,
       gchar *argv[])
@@ -40,5 +69,6 @@ main (gint argc,
   gtk_test_init (&argc, &argv, NULL);
 
   g_test_add_func("/Handy/HeaderGroup/focus", test_hdy_header_group_focus);
+  g_test_add_func("/Handy/HeaderGroup/add_remove", test_hdy_header_group_add_remove);
   return g_test_run();
 }
