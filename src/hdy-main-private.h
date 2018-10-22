fe2de144245058a67c97d44bf51b4127283134c6
Adrien Plazas <kekun.plazas@laposte.net>
2018-10-22 13:05:14 +0200

    main: Init public GObject types in hdy_init()
    This will avoid our users to manually ensure libhandy widget types are
    loaded before using them in GtkBuilder templates.
    
    Fixes https://source.puri.sm/Librem5/libhandy/issues/20

---
+++ src/hdy-main-private.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2018 Purism SPC
+ *
+ * SPDX-License-Identifier: LGPL-2.1+
+ */
+#pragma once
+
+#include "hdy-main.h"
+
+#if !defined(_HANDY_INSIDE) && !defined(HANDY_COMPILATION)
+#error "Only <handy.h> can be included directly."
+#endif
+
+G_BEGIN_DECLS
+
+void hdy_init_public_types (void);
+
+G_END_DECLS
