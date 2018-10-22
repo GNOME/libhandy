953349c7dccf9c7a84454634e7cd6607c6c5366f
Adrien Plazas <kekun.plazas@laposte.net>
2018-10-22 13:05:14 +0200

    main: Init public GObject types in hdy_init()
    This will avoid our users to manually ensure libhandy widget types are
    loaded before using them in GtkBuilder templates.
    
    Fixes https://source.puri.sm/Librem5/libhandy/issues/20

--- src/hdy-main.c
+++ src/hdy-main.c
@@ -4,7 +4,16 @@
  * SPDX-License-Identifier: LGPL-2.1+
  */
 #include "config.h"
+#include "hdy-arrows.h"
+#include "hdy-column.h"
+#include "hdy-dialer-button.h"
+#include "hdy-dialer-cycle-button.h"
+#include "hdy-dialer.h"
+#include "hdy-enums.h"
+#include "hdy-header-group.h"
+#include "hdy-leaflet.h"
 #include "hdy-main.h"
+#include "hdy-title-bar.h"
 #include <glib/gi18n.h>
 
 static gint hdy_initialized = FALSE;
@@ -17,6 +26,15 @@ static gint hdy_initialized = FALSE;
  * Before using the Handy libarary you should initialize it. This makes
  * sure translations for the Handy library are set up properly.
  */
+
+/* Initializes the public GObject types, which is needed to ensure they are
+ * discoverable, for example so they can easily be used with GtkBuilder.
+ */
+static void
+init_public_types (void)
+{
+#include "hdy-public-types.c"
+}
 
 /**
  * hdy_init:
@@ -41,6 +59,7 @@ hdy_init (int *argc, char ***argv)
   textdomain (GETTEXT_PACKAGE);
   bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
   bindtextdomain (GETTEXT_PACKAGE, LOCALEDIR);
+  init_public_types ();
 
   hdy_initialized = TRUE;
 
