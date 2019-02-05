5651e2426448b65e1258827fae4e068b734efc70
Adrien Plazas <kekun.plazas@laposte.net>
2019-02-05 16:25:40 +0100

    main: Mark hdy_init() as deprecated

--- src/hdy-main.h
+++ src/hdy-main.h
@@ -10,9 +10,15 @@
 #endif
 
 #include <glib.h>
+#include "hdy-version.h"
 
 G_BEGIN_DECLS
 
+#ifndef HANDY_DISABLE_DEPRECATED
+
+HDY_DEPRECATED_IN_0_0_8
 gboolean hdy_init(int *argc, char ***argv);
+
+#endif
 
 G_END_DECLS
