c70de5be8e2ee3bf88a252e20ade4021e216d37d
Alexander Mikhaylenko <exalm7659@gmail.com>
2019-08-26 20:26:19 +0500

    Add HdySwipeGroup
    An object that allows to synchonize swipe animations of multiple widgets.
    This can be used to sync widgets between headerbar and window content area.
    
    Signed-off-by: Alexander Mikhaylenko <exalm7659@gmail.com>

---
+++ src/hdy-swipe-group.c
@@ -0,0 +1,490 @@
+/*
+ * Copyright (C) 2019 Alexander Mikhaylenko <exalm7659@gmail.com>
+ *
+ * SPDX-License-Identifier: LGPL-2.1+
+ */
+
+#include "config.h"
+
+#include "hdy-swipe-group.h"
+#include <gtk/gtk.h>
+#include "hdy-swipeable-private.h"
+
+typedef struct
+{
+  GSList *swipeables;
+  HdySwipeable *current;
+} HdySwipeGroupPrivate;
+
+static void hdy_swipe_group_buildable_init (GtkBuildableIface *iface);
+static gboolean hdy_swipe_group_buildable_custom_tag_start (GtkBuildable  *buildable,
+                                                             GtkBuilder    *builder,
+                                                             GObject       *child,
+                                                             const gchar   *tagname,
+                                                             GMarkupParser *parser,
+                                                             gpointer      *data);
+static void hdy_swipe_group_buildable_custom_finished (GtkBuildable *buildable,
+                                                        GtkBuilder   *builder,
+                                                        GObject      *child,
+                                                        const gchar  *tagname,
+                                                        gpointer      user_data);
+
+G_DEFINE_TYPE_WITH_CODE (HdySwipeGroup, hdy_swipe_group, G_TYPE_OBJECT,
+                         G_ADD_PRIVATE (HdySwipeGroup)
+                         G_IMPLEMENT_INTERFACE (GTK_TYPE_BUILDABLE,
+                         hdy_swipe_group_buildable_init))
+
+static gboolean
+contains (HdySwipeGroup *self,
+          HdySwipeable  *swipeable)
+{
+  HdySwipeGroupPrivate *priv;
+  GSList *swipeables;
+
+  priv = hdy_swipe_group_get_instance_private (self);
+
+  for (swipeables = priv->swipeables; swipeables != NULL; swipeables = swipeables->next)
+    if (swipeables->data == swipeable)
+      return TRUE;
+
+  return FALSE;
+}
+
+static void
+swipeable_destroyed (HdySwipeGroup *self,
+                     HdySwipeable  *swipeable)
+{
+  HdySwipeGroupPrivate *priv;
+
+  g_return_if_fail (HDY_IS_SWIPE_GROUP (self));
+
+  priv = hdy_swipe_group_get_instance_private (self);
+  priv->swipeables = g_slist_remove (priv->swipeables, swipeable);
+
+  g_object_unref (self);
+}
+
+HdySwipeGroup *
+hdy_swipe_group_new (void)
+{
+  return g_object_new (HDY_TYPE_SWIPE_GROUP, NULL);
+}
+
+static void
+switch_child_cb (HdySwipeGroup *self,
+                 uint           index,
+                 gint64         duration,
+                 HdySwipeable  *swipeable)
+{
+  HdySwipeGroupPrivate *priv = hdy_swipe_group_get_instance_private (self);
+  GSList *swipeables;
+
+  if (swipeable == priv->current)
+    return;
+
+  for (swipeables = priv->swipeables; swipeables != NULL; swipeables = swipeables->next)
+    if (swipeables->data != swipeable)
+      hdy_swipeable_switch_child (swipeables->data, index, duration);
+}
+
+static void
+begin_swipe_cb (HdySwipeGroup *self,
+                HdySwipeable  *swipeable)
+{
+  HdySwipeGroupPrivate *priv = hdy_swipe_group_get_instance_private (self);
+  GSList *swipeables;
+
+  if (priv->current != NULL && priv->current != swipeable)
+    return;
+
+  priv->current = swipeable;
+
+  for (swipeables = priv->swipeables; swipeables != NULL; swipeables = swipeables->next)
+    if (swipeables->data != swipeable)
+      hdy_swipeable_begin_swipe (swipeables->data);
+}
+
+static void
+update_swipe_cb (HdySwipeGroup *self,
+                 gdouble        value,
+                 HdySwipeable  *swipeable)
+{
+  HdySwipeGroupPrivate *priv = hdy_swipe_group_get_instance_private (self);
+  GSList *swipeables;
+
+  if (swipeable != priv->current)
+    return;
+
+  for (swipeables = priv->swipeables; swipeables != NULL; swipeables = swipeables->next)
+    if (swipeables->data != swipeable)
+      hdy_swipeable_update_swipe (swipeables->data, value);
+}
+
+static void
+end_swipe_cb (HdySwipeGroup *self,
+              gint64         duration,
+              gdouble        to,
+              HdySwipeable  *swipeable)
+{
+  HdySwipeGroupPrivate *priv = hdy_swipe_group_get_instance_private (self);
+  GSList *swipeables;
+
+  if (swipeable != priv->current)
+    return;
+
+  for (swipeables = priv->swipeables; swipeables != NULL; swipeables = swipeables->next)
+    if (swipeables->data != swipeable)
+      hdy_swipeable_end_swipe (swipeables->data, duration, to);
+
+  priv->current = NULL;
+}
+
+/**
+ * hdy_swipe_group_add_swipeable:
+ * @self: a #HdySwipeGroup
+ * @swipeable: the #HdySwipeable to add
+ *
+ * When the widget is destroyed or no longer referenced elsewhere, it will
+ * be removed from the swipe group.
+ */
+void
+hdy_swipe_group_add_swipeable (HdySwipeGroup *self,
+                               HdySwipeable  *swipeable)
+{
+  HdySwipeGroupPrivate *priv;
+
+  g_return_if_fail (HDY_IS_SWIPE_GROUP (self));
+  g_return_if_fail (HDY_IS_SWIPEABLE (swipeable));
+
+  priv = hdy_swipe_group_get_instance_private (self);
+
+  g_signal_connect_swapped (swipeable, "switch-child", G_CALLBACK (switch_child_cb), self);
+  g_signal_connect_swapped (swipeable, "begin-swipe", G_CALLBACK (begin_swipe_cb), self);
+  g_signal_connect_swapped (swipeable, "update-swipe", G_CALLBACK (update_swipe_cb), self);
+  g_signal_connect_swapped (swipeable, "end-swipe", G_CALLBACK (end_swipe_cb), self);
+
+  priv->swipeables = g_slist_prepend (priv->swipeables, swipeable);
+
+  g_object_ref (self);
+
+  g_signal_connect_swapped (swipeable, "destroy", G_CALLBACK (swipeable_destroyed), self);
+}
+
+
+/**
+ * hdy_swipe_group_remove_swipeable:
+ * @self: a #HdySwipeGroup
+ * @swipeable: the #HdySwipeable to remove
+ *
+ * Removes a widget from a #HdySwipeGroup
+ **/
+void
+hdy_swipe_group_remove_swipeable (HdySwipeGroup *self,
+                                  HdySwipeable  *swipeable)
+{
+  HdySwipeGroupPrivate *priv;
+
+  g_return_if_fail (HDY_IS_SWIPE_GROUP (self));
+  g_return_if_fail (HDY_IS_SWIPEABLE (swipeable));
+  g_return_if_fail (contains (self, swipeable));
+
+  priv = hdy_swipe_group_get_instance_private (self);
+  priv->swipeables = g_slist_remove (priv->swipeables, swipeable);
+
+  g_signal_handlers_disconnect_by_data (swipeable, self);
+
+  g_object_unref (self);
+}
+
+
+/**
+ * hdy_swipe_group_get_swipeables:
+ * @self: a #HdySwipeGroup
+ *
+ * Returns the list of swipeables associated with @self.
+ *
+ * Returns:  (element-type HdySwipeable) (transfer none): a #GSList of
+ *   swipeable. The list is owned by libhandy and should not be modified.
+ **/
+GSList *
+hdy_swipe_group_get_swipeables (HdySwipeGroup *self)
+{
+  HdySwipeGroupPrivate *priv;
+
+  g_return_val_if_fail (HDY_IS_SWIPE_GROUP (self), NULL);
+  priv = hdy_swipe_group_get_instance_private (self);
+  return priv->swipeables;
+}
+
+typedef struct {
+  gchar *name;
+  gint line;
+  gint col;
+} ItemData;
+
+static void
+item_data_free (gpointer data)
+{
+  ItemData *item_data = data;
+
+  g_free (item_data->name);
+  g_free (item_data);
+}
+
+typedef struct {
+  GObject *object;
+  GtkBuilder *builder;
+  GSList *items;
+} GSListSubParserData;
+
+static void
+hdy_swipe_group_dispose (GObject *object)
+{
+  HdySwipeGroup *self = (HdySwipeGroup *)object;
+  HdySwipeGroupPrivate *priv = hdy_swipe_group_get_instance_private (self);
+
+  g_slist_free_full (priv->swipeables, (GDestroyNotify) g_object_unref);
+  priv->swipeables = NULL;
+
+  G_OBJECT_CLASS (hdy_swipe_group_parent_class)->dispose (object);
+}
+
+/*< private >
+ * @builder: a #GtkBuilder
+ * @context: the #GMarkupParseContext
+ * @parent_name: the name of the expected parent element
+ * @error: return location for an error
+ *
+ * Checks that the parent element of the currently handled
+ * start tag is @parent_name and set @error if it isn't.
+ *
+ * This is intended to be called in start_element vfuncs to
+ * ensure that element nesting is as intended.
+ *
+ * Returns: %TRUE if @parent_name is the parent element
+ */
+/* This has been copied and modified from gtkbuilder.c. */
+static gboolean
+_gtk_builder_check_parent (GtkBuilder           *builder,
+                           GMarkupParseContext  *context,
+                           const gchar          *parent_name,
+                           GError              **error)
+{
+  const GSList *stack;
+  gint line, col;
+  const gchar *parent;
+  const gchar *element;
+
+  stack = g_markup_parse_context_get_element_stack (context);
+
+  element = (const gchar *)stack->data;
+  parent = stack->next ? (const gchar *)stack->next->data : "";
+
+  if (g_str_equal (parent_name, parent) ||
+      (g_str_equal (parent_name, "object") && g_str_equal (parent, "template")))
+    return TRUE;
+
+  g_markup_parse_context_get_position (context, &line, &col);
+  g_set_error (error,
+               GTK_BUILDER_ERROR,
+               GTK_BUILDER_ERROR_INVALID_TAG,
+               ".:%d:%d Can't use <%s> here",
+               line, col, element);
+
+  return FALSE;
+}
+
+/*< private >
+ * _gtk_builder_prefix_error:
+ * @builder: a #GtkBuilder
+ * @context: the #GMarkupParseContext
+ * @error: an error
+ *
+ * Calls g_prefix_error() to prepend a filename:line:column marker
+ * to the given error. The filename is taken from @builder, and
+ * the line and column are obtained by calling
+ * g_markup_parse_context_get_position().
+ *
+ * This is intended to be called on errors returned by
+ * g_markup_collect_attributes() in a start_element vfunc.
+ */
+/* This has been copied and modified from gtkbuilder.c. */
+static void
+_gtk_builder_prefix_error (GtkBuilder           *builder,
+                           GMarkupParseContext  *context,
+                           GError              **error)
+{
+  gint line, col;
+
+  g_markup_parse_context_get_position (context, &line, &col);
+  g_prefix_error (error, ".:%d:%d ", line, col);
+}
+
+/*< private >
+ * _gtk_builder_error_unhandled_tag:
+ * @builder: a #GtkBuilder
+ * @context: the #GMarkupParseContext
+ * @object: name of the object that is being handled
+ * @element_name: name of the element whose start tag is being handled
+ * @error: return location for the error
+ *
+ * Sets @error to a suitable error indicating that an @element_name
+ * tag is not expected in the custom markup for @object.
+ *
+ * This is intended to be called in a start_element vfunc.
+ */
+/* This has been copied and modified from gtkbuilder.c. */
+static void
+_gtk_builder_error_unhandled_tag (GtkBuilder           *builder,
+                                  GMarkupParseContext  *context,
+                                  const gchar          *object,
+                                  const gchar          *element_name,
+                                  GError              **error)
+{
+  gint line, col;
+
+  g_markup_parse_context_get_position (context, &line, &col);
+  g_set_error (error,
+               GTK_BUILDER_ERROR,
+               GTK_BUILDER_ERROR_UNHANDLED_TAG,
+               ".:%d:%d Unsupported tag for %s: <%s>",
+               line, col,
+               object, element_name);
+}
+
+/* This has been copied and modified from gtksizegroup.c. */
+static void
+swipe_group_start_element (GMarkupParseContext  *context,
+                          const gchar          *element_name,
+                          const gchar         **names,
+                          const gchar         **values,
+                          gpointer              user_data,
+                          GError              **error)
+{
+  GSListSubParserData *data = (GSListSubParserData*)user_data;
+
+  if (strcmp (element_name, "widget") == 0)
+    {
+      const gchar *name;
+      ItemData *item_data;
+
+      if (!_gtk_builder_check_parent (data->builder, context, "widgets", error))
+        return;
+
+      if (!g_markup_collect_attributes (element_name, names, values, error,
+                                        G_MARKUP_COLLECT_STRING, "name", &name,
+                                        G_MARKUP_COLLECT_INVALID))
+        {
+          _gtk_builder_prefix_error (data->builder, context, error);
+          return;
+        }
+
+      item_data = g_new (ItemData, 1);
+      item_data->name = g_strdup (name);
+      g_markup_parse_context_get_position (context, &item_data->line, &item_data->col);
+      data->items = g_slist_prepend (data->items, item_data);
+    }
+  else if (strcmp (element_name, "widgets") == 0)
+    {
+      if (!_gtk_builder_check_parent (data->builder, context, "object", error))
+        return;
+
+      if (!g_markup_collect_attributes (element_name, names, values, error,
+                                        G_MARKUP_COLLECT_INVALID, NULL, NULL,
+                                        G_MARKUP_COLLECT_INVALID))
+        _gtk_builder_prefix_error (data->builder, context, error);
+    }
+  else
+    {
+      _gtk_builder_error_unhandled_tag (data->builder, context,
+                                        "GtkSizeGroup", element_name,
+                                        error);
+    }
+}
+
+
+/* This has been copied and modified from gtksizegroup.c. */
+static const GMarkupParser swipe_group_parser =
+  {
+    swipe_group_start_element
+  };
+
+/* This has been copied and modified from gtksizegroup.c. */
+static gboolean
+hdy_swipe_group_buildable_custom_tag_start (GtkBuildable  *buildable,
+                                             GtkBuilder    *builder,
+                                             GObject       *child,
+                                             const gchar   *tagname,
+                                             GMarkupParser *parser,
+                                             gpointer      *parser_data)
+{
+  GSListSubParserData *data;
+
+  if (child)
+    return FALSE;
+
+  if (strcmp (tagname, "widgets") == 0)
+    {
+      data = g_slice_new0 (GSListSubParserData);
+      data->items = NULL;
+      data->object = G_OBJECT (buildable);
+      data->builder = builder;
+
+      *parser = swipe_group_parser;
+      *parser_data = data;
+
+      return TRUE;
+    }
+
+  return FALSE;
+}
+
+/* This has been copied and modified from gtksizegroup.c. */
+static void
+hdy_swipe_group_buildable_custom_finished (GtkBuildable *buildable,
+                                            GtkBuilder   *builder,
+                                            GObject      *child,
+                                            const gchar  *tagname,
+                                            gpointer      user_data)
+{
+  GSList *l;
+  GSListSubParserData *data;
+  GObject *object;
+
+  if (strcmp (tagname, "widgets") != 0)
+    return;
+
+  data = (GSListSubParserData*)user_data;
+  data->items = g_slist_reverse (data->items);
+
+  for (l = data->items; l; l = l->next)
+    {
+      ItemData *item_data = l->data;
+      object = gtk_builder_get_object (builder, item_data->name);
+      if (!object)
+        continue;
+      hdy_swipe_group_add_swipeable (HDY_SWIPE_GROUP (data->object), HDY_SWIPEABLE (object));
+    }
+  g_slist_free_full (data->items, item_data_free);
+  g_slice_free (GSListSubParserData, data);
+}
+
+static void
+hdy_swipe_group_class_init (HdySwipeGroupClass *klass)
+{
+  GObjectClass *object_class = G_OBJECT_CLASS (klass);
+
+  object_class->dispose = hdy_swipe_group_dispose;
+}
+
+static void
+hdy_swipe_group_init (HdySwipeGroup *self)
+{
+}
+
+static void
+hdy_swipe_group_buildable_init (GtkBuildableIface *iface)
+{
+  iface->custom_tag_start = hdy_swipe_group_buildable_custom_tag_start;
+  iface->custom_finished = hdy_swipe_group_buildable_custom_finished;
+}
