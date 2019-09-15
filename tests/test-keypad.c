adde7f66079d54fee934295ce22f0be7280d0393
Julian Sparber <julian@sparber.net>
2019-09-15 15:01:54 +0200

    Keypad: Add a general keypad
    This is based on HdyDialer, but with more flexible API.
    It also adds a GtkEntry which can be used as the focus widget,
    it has the key-press-event already connected and it grabs focus once
    it's mapped.
    
    The new keypad also allows to set a custom button in the left and right
    lower corner.

---
+++ tests/test-keypad.c
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2017 Purism SPC
+ *
+ * SPDX-License-Identifier: LGPL-2.1+
+ */
+
+#define HANDY_USE_UNSTABLE_API
+#include <handy.h>
+
+gint
+main (gint argc,
+      gchar *argv[])
+{
+  gtk_test_init (&argc, &argv, NULL);
+  hdy_init (&argc, &argv);
+
+  return g_test_run ();
+}
