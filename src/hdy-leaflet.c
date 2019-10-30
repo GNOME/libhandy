81224042c964c0f010d290d989a62e5777142939
Adrien Plazas <kekun.plazas@laposte.net>
2019-10-30 12:16:02 +0100

    leaflet: Add version info to the over/under mode transitions
    These were forgotten in the previous commit.

--- src/hdy-leaflet.c
+++ src/hdy-leaflet.c
@@ -35,8 +35,8 @@
  * HdyLeafletModeTransitionType:
  * @HDY_LEAFLET_MODE_TRANSITION_TYPE_NONE: No transition
  * @HDY_LEAFLET_MODE_TRANSITION_TYPE_SLIDE: Slide from left, right, up or down according to the orientation, text direction and the children order
- * @HDY_LEAFLET_MODE_TRANSITION_TYPE_OVER: Cover the old page or uncover the new page, sliding from or towards the end according to orientation, text direction and children order
- * @HDY_LEAFLET_MODE_TRANSITION_TYPE_UNDER: Uncover the new page or cover the old page, sliding from or towards the start according to orientation, text direction and children order
+ * @HDY_LEAFLET_MODE_TRANSITION_TYPE_OVER: Cover the old page or uncover the new page, sliding from or towards the end according to orientation, text direction and children order. Since 0.0.12.
+ * @HDY_LEAFLET_MODE_TRANSITION_TYPE_UNDER: Uncover the new page or cover the old page, sliding from or towards the start according to orientation, text direction and children order. Since 0.0.12.
  *
  * These enumeration values describe the possible transitions between pages in a
  * #HdyLeaflet widget.
@@ -49,8 +49,8 @@
  * @HDY_LEAFLET_CHILD_TRANSITION_TYPE_NONE: No transition
  * @HDY_LEAFLET_CHILD_TRANSITION_TYPE_CROSSFADE: A cross-fade
  * @HDY_LEAFLET_CHILD_TRANSITION_TYPE_SLIDE: Slide from left, right, up or down according to the orientation, text direction and the children order
- * @HDY_LEAFLET_CHILD_TRANSITION_TYPE_OVER: Cover the old page or uncover the new page, sliding from or towards the end according to orientation, text direction and children order
- * @HDY_LEAFLET_CHILD_TRANSITION_TYPE_UNDER: Uncover the new page or cover the old page, sliding from or towards the start according to orientation, text direction and children order
+ * @HDY_LEAFLET_CHILD_TRANSITION_TYPE_OVER: Cover the old page or uncover the new page, sliding from or towards the end according to orientation, text direction and children order. Since 0.0.12.
+ * @HDY_LEAFLET_CHILD_TRANSITION_TYPE_UNDER: Uncover the new page or cover the old page, sliding from or towards the start according to orientation, text direction and children order. Since 0.0.12.
  *
  * These enumeration values describe the possible transitions between pages in a
  * #HdyLeaflet widget.
