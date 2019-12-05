d8b1830486a649e1d4737bf15fe0415342b7d5c1
Alexander Mikhaylenko <alexm@gnome.org>
2019-12-05 19:46:36 +0500

    paginator: Add 'mouse-drag' property
    Usually we don't want this, because there's scrolling. However, phosh
    still needs this for lockscreen, hence optionally allow it.
    
    Signed-off-by: Alexander Mikhaylenko <alexm@gnome.org>

--- src/hdy-paginator.h
+++ src/hdy-paginator.h
@@ -69,4 +69,8 @@ guint         hdy_paginator_get_animation_duration (HdyPaginator *self);
 void          hdy_paginator_set_animation_duration (HdyPaginator *self,
                                                     guint         duration);
 
+gboolean      hdy_paginator_get_mouse_drag (HdyPaginator *self);
+void          hdy_paginator_set_mouse_drag (HdyPaginator *self,
+                                            gboolean      mouse_drag);
+
 G_END_DECLS
