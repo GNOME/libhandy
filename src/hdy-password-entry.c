135d2ac61512eeeafe66d8a7eed020799567fb58
Ujjwal Kumar <ujjwalkumar0501@gmail.com>
2020-03-06 04:30:23 +0530

    password-entry: Basic working implementation

--- src/hdy-password-entry.c
+++ src/hdy-password-entry.c
@@ -11,20 +11,90 @@
 
 typedef struct
 {
+  guint timeout_id;
   gboolean show_characters;
 } HdyPasswordEntryPrivate;
 
 G_DEFINE_TYPE_WITH_PRIVATE (HdyPasswordEntry, hdy_password_entry, GTK_TYPE_ENTRY);
 
+#define REVEAL_TIMEOUT 2000
+
+static gboolean
+timeout_cb (gpointer user_data)
+{
+  HdyPasswordEntry *entry = user_data;
+  HdyPasswordEntryPrivate *priv = hdy_password_entry_get_instance_private (entry);
+
+  priv->timeout_id = 0;
+  gtk_entry_set_visibility (GTK_ENTRY (entry), FALSE);
+
+  return TRUE;
+}
 
 static void
-hdy_password_entry_class_init (HdyPasswordEntryClass *klass) {
+set_timeout (HdyPasswordEntry *entry)
+{
+  HdyPasswordEntryPrivate *priv = hdy_password_entry_get_instance_private (entry);
+
+  if (priv->timeout_id > 0)
+    g_source_remove (priv->timeout_id);
+  priv->timeout_id = g_timeout_add (REVEAL_TIMEOUT,
+                                    timeout_cb,
+                                    entry);
+  g_source_set_name_by_id (priv->timeout_id, "[gtk] gtk_search_entry_changed_timeout_cb");
+}
+
+static void
+secondary_icon_clicked_cb (HdyPasswordEntry              *entry,
+                           GtkEntryIconPosition           icon_pos,
+                           GdkEvent                      *event,
+                           gpointer                       user_data)
+{
+  HdyPasswordEntryPrivate *priv = hdy_password_entry_get_instance_private (entry);
+
+  if (icon_pos == GTK_ENTRY_ICON_SECONDARY)
+  {
+    if (priv->timeout_id > 0)
+    {
+      g_source_remove (priv->timeout_id);
+      gtk_entry_set_visibility (GTK_ENTRY (entry), FALSE);
+      priv->timeout_id = 0;
+    }
+    else
+    {
+      gtk_entry_set_visibility (GTK_ENTRY (entry), TRUE);
+      set_timeout (entry);
+    }
+  }
+}
+
+static void
+hdy_password_entry_finalize (GObject *object)
+{
+  // just for future use
+
+  G_OBJECT_CLASS (hdy_password_entry_parent_class)->finalize (object);
 
 }
 
 static void
-hdy_password_entry_init (HdyPasswordEntry *self) {
+hdy_password_entry_class_init (HdyPasswordEntryClass *klass)
+{
+  GObjectClass *object_class = G_OBJECT_CLASS (klass);
+  GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
 
+  object_class->finalize = hdy_password_entry_finalize;
+
+  gtk_widget_class_set_template_from_resource (widget_class,
+                                               "/sm/puri/handy/ui/hdy-password-entry.ui");
+
+  gtk_widget_class_bind_template_callback (widget_class, secondary_icon_clicked_cb);
+}
+
+static void
+hdy_password_entry_init (HdyPasswordEntry *entry)
+{
+  gtk_widget_init_template (GTK_WIDGET (entry));
 }
 
 GtkWidget *
