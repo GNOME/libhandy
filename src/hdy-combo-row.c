9f3e65528aab5e83a5d8028aa21f13fa42cc8242
Adrien Plazas <kekun.plazas@laposte.net>
2020-03-13 11:43:40 +0100

    combo-row: Make it activatable when it has a bound model
    This makes HdyComboRow unactivatable by default and when it has no bound
    model, but make it activatable as soon as it has a model bound to it.

--- src/hdy-combo-row.c
+++ src/hdy-combo-row.c
@@ -285,6 +285,8 @@ destroy_model (HdyComboRow *self)
   priv->create_current_widget_func = NULL;
   priv->create_widget_func_data = NULL;
   priv->create_widget_func_data_free_func = NULL;
+
+  gtk_list_box_row_set_activatable (GTK_LIST_BOX_ROW (self), FALSE);
 }
 
 static void
@@ -558,6 +560,8 @@ hdy_combo_row_bind_model (HdyComboRow                *self,
     priv->selected_index = 0;
 
   gtk_list_box_bind_model (priv->list, model, create_list_widget, self, create_list_widget_data_free);
+
+  gtk_list_box_row_set_activatable (GTK_LIST_BOX_ROW (self), TRUE);
 
   update (self);
 
