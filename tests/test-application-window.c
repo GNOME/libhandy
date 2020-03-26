19dcf05649f2974d70032c633ea842e061e7fc97
Alexander Mikhaylenko <alexm@gnome.org>
2020-03-27 03:52:20 +0500

    tests: Add a HdyApplicationWindow test
    Signed-off-by: Alexander Mikhaylenko <alexm@gnome.org>

---
+++ tests/test-application-window.c
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2020 Alexander Mikhaylenko <alexm@gnome.org>
+ *
+ * SPDX-License-Identifier: LGPL-2.1+
+ */
+
+#define HANDY_USE_UNSTABLE_API
+#include <handy.h>
+
+
+static void
+test_hdy_application_window_styles (void)
+{
+  g_autoptr (GtkWidget) window = NULL;
+  GtkWidget *box;
+  GtkStyleContext *context;
+  GtkAllocation alloc = { 0 };
+
+  window = g_object_ref_sink (hdy_application_window_new ());
+  g_assert_nonnull (window);
+
+  context = gtk_widget_get_style_context (window);
+  g_assert_nonnull (context);
+
+  g_assert_false (gtk_style_context_has_class (context, "unified"));
+
+  box = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 0);
+  gtk_window_set_titlebar (GTK_WINDOW (window), box);
+
+  alloc.width = 300;
+  alloc.height = 600;
+  gtk_widget_size_allocate (window, &alloc);
+
+  g_assert_true (gtk_style_context_has_class (context, "unified"));
+}
+
+
+gint
+main (gint argc,
+      gchar *argv[])
+{
+  gtk_test_init (&argc, &argv, NULL);
+
+  g_test_add_func("/Handy/ApplicationWindow/add", test_hdy_application_window_styles);
+
+  return g_test_run();
+}
