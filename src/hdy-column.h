117b21f54070eb44b2e6c078c29e4719adc6440f
Adrien Plazas <kekun.plazas@laposte.net>
2020-06-08 10:30:46 +0200

    Make symbols explicitly public, and implicitly private
    Fixes https://gitlab.gnome.org/GNOME/libhandy/-/issues/177

--- src/hdy-column.h
+++ src/hdy-column.h
@@ -10,19 +10,27 @@
 #error "Only <handy.h> can be included directly."
 #endif
 
+#include "hdy-version.h"
+
 #include <gtk/gtk.h>
 
 G_BEGIN_DECLS
 
 #define HDY_TYPE_COLUMN (hdy_column_get_type())
 
+HDY_AVAILABLE_IN_ALL
 G_DECLARE_FINAL_TYPE (HdyColumn, hdy_column, HDY, COLUMN, GtkBin)
 
+HDY_AVAILABLE_IN_ALL
 GtkWidget *hdy_column_new (void);
+HDY_AVAILABLE_IN_ALL
 gint hdy_column_get_maximum_width (HdyColumn *self);
+HDY_AVAILABLE_IN_ALL
 void hdy_column_set_maximum_width (HdyColumn *self,
                                    gint       maximum_width);
+HDY_AVAILABLE_IN_ALL
 gint hdy_column_get_linear_growth_width (HdyColumn *self);
+HDY_AVAILABLE_IN_ALL
 void hdy_column_set_linear_growth_width (HdyColumn *self,
                                          gint       linear_growth_width);
 
--- src/hdy-header-group.h
+++ src/hdy-header-group.h
@@ -10,12 +10,15 @@
 #error "Only <handy.h> can be included directly."
 #endif
 
+#include "hdy-version.h"
+
 #include <gtk/gtk.h>
 
 G_BEGIN_DECLS
 
 #define HDY_TYPE_HEADER_GROUP (hdy_header_group_get_type())
 
+HDY_AVAILABLE_IN_ALL
 G_DECLARE_DERIVABLE_TYPE (HdyHeaderGroup, hdy_header_group, HDY, HEADER_GROUP, GObject)
 
 /**
@@ -27,15 +30,21 @@ struct _HdyHeaderGroupClass
   GObjectClass parent_class;
 };
 
+HDY_AVAILABLE_IN_ALL
 HdyHeaderGroup *hdy_header_group_new (void);
 
+HDY_AVAILABLE_IN_ALL
 void hdy_header_group_add_header_bar (HdyHeaderGroup *self,
                                       GtkHeaderBar   *header_bar);
 
+HDY_AVAILABLE_IN_ALL
 GtkHeaderBar *hdy_header_group_get_focus         (HdyHeaderGroup *self);
+HDY_AVAILABLE_IN_ALL
 void          hdy_header_group_set_focus         (HdyHeaderGroup *self,
                                                   GtkHeaderBar   *header_bar);
+HDY_AVAILABLE_IN_ALL
 GSList *      hdy_header_group_get_header_bars   (HdyHeaderGroup *self);
+HDY_AVAILABLE_IN_ALL
 void          hdy_header_group_remove_header_bar (HdyHeaderGroup *self,
                                                   GtkHeaderBar   *header_bar);
 
