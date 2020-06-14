fff5c4e687a3cb1c209c21cab5ff0b19a0052486
Alexander Mikhaylenko <alexm@gnome.org>
2020-06-15 01:37:46 +0500

    swipeable: Make completely public
    Signed-off-by: Alexander Mikhaylenko <alexm@gnome.org>

--- src/hdy-swipeable.h
+++ src/hdy-swipeable.h
@@ -56,12 +56,39 @@ struct _HdySwipeableInterface
 
   gdouble   (*get_distance)        (HdySwipeable *self);
   void      (*get_range)           (HdySwipeable *self,
-                                    gdouble      *min_progress,
-                                    gdouble      *max_progress);
+                                    gdouble      *lower,
+                                    gdouble      *upper);
   gdouble * (*get_snap_points)     (HdySwipeable *self,
                                     gint         *n_snap_points);
   gdouble   (*get_progress)        (HdySwipeable *self);
   gdouble   (*get_cancel_progress) (HdySwipeable *self);
 };
+
+void hdy_swipeable_switch_child (HdySwipeable *self,
+                                 guint         index,
+                                 gint64        duration);
+void hdy_swipeable_start_swipe  (HdySwipeable           *self,
+                                 HdyNavigationDirection  direction,
+                                 gboolean                direct,
+                                 gint                    start_x,
+                                 gint                    start_y);
+void hdy_swipeable_update_swipe (HdySwipeable *self,
+                                 gdouble       value);
+void hdy_swipeable_end_swipe    (HdySwipeable *self,
+                                 gint64        duration,
+                                 gdouble       to);
+
+void hdy_swipeable_emit_child_switched (HdySwipeable *self,
+                                        guint         index,
+                                        gint64        duration);
+
+gdouble  hdy_swipeable_get_distance        (HdySwipeable *self);
+void     hdy_swipeable_get_range           (HdySwipeable *self,
+                                            gdouble      *lower,
+                                            gdouble      *upper);
+gdouble *hdy_swipeable_get_snap_points     (HdySwipeable *self,
+                                            gint         *n_snap_points);
+gdouble  hdy_swipeable_get_progress        (HdySwipeable *self);
+gdouble  hdy_swipeable_get_cancel_progress (HdySwipeable *self);
 
 G_END_DECLS
