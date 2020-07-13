177786987178a855dd5f4950fa2a777473b6ceb4
Alexander Mikhaylenko <alexm@gnome.org>
2020-07-14 00:55:33 +0500

    carousel: Remove indicators
    They have been split into separate widgets, so can be removed now.
    
    Signed-off-by: Alexander Mikhaylenko <alexm@gnome.org>

--- src/hdy-carousel.h
+++ src/hdy-carousel.h
@@ -11,19 +11,12 @@
 #endif
 
 #include <gtk/gtk.h>
-#include "hdy-enums.h"
 
 G_BEGIN_DECLS
 
 #define HDY_TYPE_CAROUSEL (hdy_carousel_get_type())
 
 G_DECLARE_FINAL_TYPE (HdyCarousel, hdy_carousel, HDY, CAROUSEL, GtkEventBox)
-
-typedef enum {
-  HDY_CAROUSEL_INDICATOR_STYLE_NONE,
-  HDY_CAROUSEL_INDICATOR_STYLE_DOTS,
-  HDY_CAROUSEL_INDICATOR_STYLE_LINES,
-} HdyCarouselIndicatorStyle;
 
 GtkWidget      *hdy_carousel_new (void);
 
@@ -48,18 +41,6 @@ gdouble         hdy_carousel_get_position (HdyCarousel *self);
 gboolean        hdy_carousel_get_interactive (HdyCarousel *self);
 void            hdy_carousel_set_interactive (HdyCarousel *self,
                                               gboolean     interactive);
-
-HdyCarouselIndicatorStyle hdy_carousel_get_indicator_style (HdyCarousel *self);
-void            hdy_carousel_set_indicator_style (HdyCarousel               *self,
-                                                  HdyCarouselIndicatorStyle  style);
-
-guint           hdy_carousel_get_indicator_spacing (HdyCarousel *self);
-void            hdy_carousel_set_indicator_spacing (HdyCarousel *self,
-                                                    guint        spacing);
-
-gboolean        hdy_carousel_get_center_content (HdyCarousel *self);
-void            hdy_carousel_set_center_content (HdyCarousel *self,
-                                                 gboolean     center_content);
 
 guint           hdy_carousel_get_spacing (HdyCarousel *self);
 void            hdy_carousel_set_spacing (HdyCarousel *self,
