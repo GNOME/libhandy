9427d25b8d2b1cc74a529ac7ce640965fbed021a
Adrien Plazas <kekun.plazas@laposte.net>
2020-07-15 12:36:07 +0200

    stackable-box: Swipe to higher children only from borders
    This makes swiping forward with the over transition or back with the
    under transition possible only from the border, while still allowing to
    swipe from anywhere otherwise.
    
    Fixes https://gitlab.gnome.org/GNOME/libhandy/-/issues/167

--- src/hdy-swipe-tracker.c
+++ src/hdy-swipe-tracker.c
@@ -140,22 +140,25 @@ get_range (HdySwipeTracker *self,
 
 static void
 gesture_prepare (HdySwipeTracker        *self,
-                 HdyNavigationDirection  direction)
+                 HdyNavigationDirection  direction,
+                 gboolean                is_drag)
 {
   GdkRectangle rect;
 
   if (self->state != HDY_SWIPE_TRACKER_STATE_NONE)
     return;
 
-  hdy_swipeable_get_swipe_area (self->swipeable, direction, &rect);
+  if (is_drag) {
+    hdy_swipeable_get_swipe_area (self->swipeable, direction, &rect);
 
-  if (self->start_x < rect.x ||
-      self->start_x >= rect.x + rect.width ||
-      self->start_y < rect.y ||
-      self->start_y >= rect.y + rect.height) {
-    self->state = HDY_SWIPE_TRACKER_STATE_REJECTED;
+    if (self->start_x < rect.x ||
+        self->start_x >= rect.x + rect.width ||
+        self->start_y < rect.y ||
+        self->start_y >= rect.y + rect.height) {
+      self->state = HDY_SWIPE_TRACKER_STATE_REJECTED;
 
-    return;
+      return;
+    }
   }
 
   hdy_swipe_tracker_emit_begin_swipe (self, direction, TRUE);
@@ -345,7 +348,7 @@ drag_update_cb (HdySwipeTracker *self,
 
   if (self->state == HDY_SWIPE_TRACKER_STATE_NONE) {
     if (is_vertical == is_offset_vertical)
-      gesture_prepare (self, offset > 0 ? HDY_NAVIGATION_DIRECTION_FORWARD : HDY_NAVIGATION_DIRECTION_BACK);
+      gesture_prepare (self, offset > 0 ? HDY_NAVIGATION_DIRECTION_FORWARD : HDY_NAVIGATION_DIRECTION_BACK, TRUE);
     else
       gtk_gesture_set_state (self->touch_gesture, GTK_EVENT_SEQUENCE_DENIED);
     return;
@@ -476,7 +479,7 @@ handle_scroll_event (HdySwipeTracker *self,
                                           event_x, event_y,
                                           &self->start_x, &self->start_y);
 
-        gesture_prepare (self, delta > 0 ? HDY_NAVIGATION_DIRECTION_FORWARD : HDY_NAVIGATION_DIRECTION_BACK);
+        gesture_prepare (self, delta > 0 ? HDY_NAVIGATION_DIRECTION_FORWARD : HDY_NAVIGATION_DIRECTION_BACK, FALSE);
       }
     } else {
       self->is_scrolling = TRUE;
