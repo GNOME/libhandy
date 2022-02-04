7ec50e608a39ee5bae3296eb5ca1a9287f9b6e73
Alexander Mikhaylenko <alexm@gnome.org>
2022-02-04 16:02:20 +0500

    doc: Only replace urls for gtk-doc-based docs

--- doc.sh
+++ doc.sh
@@ -38,15 +38,15 @@ for REF in $LEGACY_REFS; do
 
   rm "$REF.zip"
   rm -rf "$REF"
+
+  find $DOC_DIR/$API_VERSION -type f -print0 | xargs -0 sed -i 's|\.\./gdk3/|https://developer.gnome.org/gdk3/stable/|g'
+  find $DOC_DIR/$API_VERSION -type f -print0 | xargs -0 sed -i 's|\.\./gdk-pixbuf/|https://developer.gnome.org/gdk-pixbuf/stable/|g'
+  find $DOC_DIR/$API_VERSION -type f -print0 | xargs -0 sed -i 's|\.\./gio/|https://developer.gnome.org/gio/stable/|g'
+  find $DOC_DIR/$API_VERSION -type f -print0 | xargs -0 sed -i 's|\.\./glib/|https://developer.gnome.org/glib/stable/|g'
+  find $DOC_DIR/$API_VERSION -type f -print0 | xargs -0 sed -i 's|\.\./gobject/|https://developer.gnome.org/gobject/stable/|g'
+  find $DOC_DIR/$API_VERSION -type f -print0 | xargs -0 sed -i 's|\.\./gtk3/|https://developer.gnome.org/gtk3/stable/|g'
+  find $DOC_DIR/$API_VERSION -type f -print0 | xargs -0 sed -i 's|\.\./pango/|https://developer.gnome.org/pango/stable/|g'
 done
 
 ln -s $LATEST_STABLE_1 $DOC_DIR/1-latest
 ln -s main $DOC_DIR/master
-
-find $DOC_DIR -type f -print0 | xargs -0 sed -i 's|\.\./gdk3/|https://developer.gnome.org/gdk3/stable/|g'
-find $DOC_DIR -type f -print0 | xargs -0 sed -i 's|\.\./gdk-pixbuf/|https://developer.gnome.org/gdk-pixbuf/stable/|g'
-find $DOC_DIR -type f -print0 | xargs -0 sed -i 's|\.\./gio/|https://developer.gnome.org/gio/stable/|g'
-find $DOC_DIR -type f -print0 | xargs -0 sed -i 's|\.\./glib/|https://developer.gnome.org/glib/stable/|g'
-find $DOC_DIR -type f -print0 | xargs -0 sed -i 's|\.\./gobject/|https://developer.gnome.org/gobject/stable/|g'
-find $DOC_DIR -type f -print0 | xargs -0 sed -i 's|\.\./gtk3/|https://developer.gnome.org/gtk3/stable/|g'
-find $DOC_DIR -type f -print0 | xargs -0 sed -i 's|\.\./pango/|https://developer.gnome.org/pango/stable/|g'
