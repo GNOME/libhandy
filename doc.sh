208ca94ca2d2b1faffbae6ed90a1e92f49b7ccc9
Alice Mikhaylenko <alicem@gnome.org>
2025-04-09 00:39:40 +0400

    pages: Remove 1.x docs besides from main
    They don't work anyway, let's just have main and that's it. It's not like
    libhandy is actively developed anyway.

--- doc.sh
+++ doc.sh
@@ -3,20 +3,7 @@
 DOC_DIR=public/doc
 REFS="
 main
-libhandy-1-6
-libhandy-1-8
 "
-
-LEGACY_REFS="
-libhandy-1-0
-libhandy-1-2
-libhandy-1-4
-"
-
-LATEST_STABLE_1=1.8
-
-IFS='
-'
 
 mkdir -p $DOC_DIR
 
@@ -31,24 +18,10 @@ for REF in $REFS; do
   rm -rf "$REF"
 done
 
-for REF in $LEGACY_REFS; do
-  API_VERSION=`echo $REF | sed 's/libhandy-\([0-9][0-9]*\)-\([0-9][0-9]*\)/\1.\2/'`
-
-  curl -L --output "$REF.zip" "https://gitlab.gnome.org/GNOME/libhandy/-/jobs/artifacts/$REF/download?job=build-gtkdoc"
-  unzip -d "$REF" "$REF.zip"
-  mv "$REF/_reference" $DOC_DIR/$API_VERSION
-
-  rm "$REF.zip"
-  rm -rf "$REF"
-
-  find $DOC_DIR/$API_VERSION -type f -print0 | xargs -0 sed -i 's|\.\./gdk3/|https://developer.gnome.org/gdk3/stable/|g'
-  find $DOC_DIR/$API_VERSION -type f -print0 | xargs -0 sed -i 's|\.\./gdk-pixbuf/|https://developer.gnome.org/gdk-pixbuf/stable/|g'
-  find $DOC_DIR/$API_VERSION -type f -print0 | xargs -0 sed -i 's|\.\./gio/|https://developer.gnome.org/gio/stable/|g'
-  find $DOC_DIR/$API_VERSION -type f -print0 | xargs -0 sed -i 's|\.\./glib/|https://developer.gnome.org/glib/stable/|g'
-  find $DOC_DIR/$API_VERSION -type f -print0 | xargs -0 sed -i 's|\.\./gobject/|https://developer.gnome.org/gobject/stable/|g'
-  find $DOC_DIR/$API_VERSION -type f -print0 | xargs -0 sed -i 's|\.\./gtk3/|https://developer.gnome.org/gtk3/stable/|g'
-  find $DOC_DIR/$API_VERSION -type f -print0 | xargs -0 sed -i 's|\.\./pango/|https://developer.gnome.org/pango/stable/|g'
-done
-
-ln -s $LATEST_STABLE_1 $DOC_DIR/1-latest
+ln -s main $DOC_DIR/1-latest
+ln -s main $DOR_DIR/1.8
+ln -s main $DOR_DIR/1.6
+ln -s main $DOR_DIR/1.4
+ln -s main $DOR_DIR/1.2
+ln -s main $DOR_DIR/1.0
 ln -s main $DOC_DIR/master
